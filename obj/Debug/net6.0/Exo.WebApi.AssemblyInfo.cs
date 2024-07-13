62d0a5a7a3ee2a5932c86a79b1b56ac0f524afe6
aquabode <l.magno911@gmail.com>
2024-07-12 23:23:28 -0300

    crud

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50d8dd7f83f11199f8d99760ab7dfe289bd2bb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077e51e7fcddbbbff6d912548c15377e9a340b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
